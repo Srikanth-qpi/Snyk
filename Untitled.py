e3d7b3598586448d149f4dc86047006e8e93e084
Srikanth-qpi <126075454+Srikanth-qpi@users.noreply.github.com>
2023-02-23 12:06:48 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+# content of test_class.py
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, "check")
+
+
+# In[29]:
+
+
+x.TestClass()
+
+
+# In[ ]:
+
+
+
+
